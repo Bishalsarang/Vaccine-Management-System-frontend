a15975cca0e17629a5f429c55bd58c8237a46e27
Bishalsarang <bishalsarang@gmail.com>
2022-12-29 17:27:52 +0545

    Update the design for sidebar.

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,36 +1,104 @@
 import { NavLink } from 'react-router-dom';
+import {
+  List,
+  Drawer,
+  Divider,
+  Tooltip,
+  ListItem,
+  IconButton,
+  ListItemIcon,
+  ListItemText,
+} from '@mui/material';
+import { Menu as MenuIcon } from '@mui/icons-material';
 
 import { IconType } from 'react-icons/lib/cjs/iconBase';
 
 interface SidebarProps {
   navigationItems: { label: string; path: string; icon: IconType }[];
+  isOpen: boolean;
+  onClose?: () => void;
+  onOpen?: () => void;
 }
 
-export default function Sidebar({ navigationItems = [] }: SidebarProps) {
-  return (
-    <div className="h-full w-96  bg-slate-900 p-4">
-      <div className="mb-4 flex items-center">
-        <span className="text-center text-2xl font-bold text-blue-400 shadow-xl">
-          Vaccine Management System
-        </span>
-      </div>
+export default function Sidebar({
+  isOpen,
+  onOpen,
+  onClose,
+  navigationItems = [],
+}: SidebarProps) {
+  // This styling will be applied to a <NavLink> when the
+  // route that it links to is currently selected.
+  const activeStyle = {
+    textDecoration: 'underline',
+    background: 'red',
+  };
 
-      <nav className="text-2xl font-semibold text-white">
-        {navigationItems.map((item) => (
-          <NavLink
-            to={item.path}
-            key={item.label}
-            className="flex items-center p-3 hover:bg-gray-800 active:bg-gray-100"
-          >
-            <>
-              <div className="flex items-center gap-2">
-                <item.icon />
-                {item.label}
-              </div>
-            </>
-          </NavLink>
-        ))}
-      </nav>
+  return (
+    <div className="bg-white shadow-md">
+      {!isOpen && (
+        <div>
+          <div className="mb-4 flex items-center p-4">
+            <span className="max-w-xs text-center text-2xl font-bold text-blue-400 ">
+              VMS
+            </span>
+
+            <IconButton onClick={onOpen}>
+              <MenuIcon />
+            </IconButton>
+          </div>
+          <Divider />
+
+          <div className="flex flex-col items-center">
+            <List>
+              {navigationItems.map((item) => (
+                <NavLink
+                  to={item.path}
+                  className={({ isActive }) => {
+                    console.log(isActive);
+                    return isActive ? 'Mui-selected bg-red-400' : '';
+                  }}
+                  key={item.label}
+                >
+                  <Tooltip title={item.label} placement="right-start">
+                    <ListItem className="">
+                      <ListItemIcon className="p-2">
+                        <item.icon fontSize={24} size={24} />
+                      </ListItemIcon>
+                    </ListItem>
+                  </Tooltip>
+                </NavLink>
+              ))}
+            </List>
+          </div>
+        </div>
+      )}
+      <Drawer anchor="left" open={isOpen} onClose={onClose}>
+        <div>
+          <div className="mb-4 flex items-center p-4">
+            <span className="max-w-xs text-center text-2xl font-bold text-blue-400 ">
+              Vaccine Management System
+            </span>
+          </div>
+          <Divider />
+          <List>
+            {navigationItems.map((item) => (
+              <NavLink to={item.path} key={item.label}>
+                <ListItem className="">
+                  <ListItemIcon>
+                    <item.icon fontSize={24} size={24} />
+                  </ListItemIcon>
+                  <ListItemText primary={item.label} />
+                </ListItem>
+              </NavLink>
+            ))}
+          </List>
+        </div>
+      </Drawer>
+      {isOpen && (
+        <IconButton onClick={onClose}>
+          <MenuIcon />
+        </IconButton>
+      )}
     </div>
   );
 }
--- src/pages/DashboardPage/DashboardPage.tsx
+++ src/pages/DashboardPage/DashboardPage.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import Sidebar from '../../components/Sidebar';
 
 import { SIDEBAR_NAV_ITEMS } from '../../constants/base.constants';
@@ -5,10 +6,27 @@ import { SIDEBAR_NAV_ITEMS } from '../../constants/base.constants';
 import ContentPage from '../ContentPage';
 
 export default function DashboardPage() {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const handleSidebarClose = () => {
+    setIsSidebarOpen(false);
+  };
+
+  const handleSidebarOpen = () => {
+    setIsSidebarOpen(true);
+  };
+
   return (
     <div className="flex h-screen">
-      <Sidebar navigationItems={SIDEBAR_NAV_ITEMS} />
-      <ContentPage />
+      <Sidebar
+        isOpen={isSidebarOpen}
+        onOpen={handleSidebarOpen}
+        onClose={handleSidebarClose}
+        navigationItems={SIDEBAR_NAV_ITEMS}
+      />
+      <div className="w-full flex-1 overflow-auto">
+        <ContentPage />
+      </div>
     </div>
   );
 }
